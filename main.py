ddcf57e5a4aaca822c8c85b4b97f839f6b38ee12
Viktor <viktor_rein@outlook.de>
2025-07-24 17:19:47 +0200

    push_1

---
+++ main.py
@@ -0,0 +1,15 @@
+# Autor: Viktor Rein
+# Datum: 2025-07-24  
+# Projekt: sqlite-notebook
+# Beschreibung: <Platzhalter für Beschreibung des Projekts>
+# Version: 0.1
+# Weitere Informationen: <Platzhalter für weitere Infos, z.B. Lizenz, Kontakt>
+
+def main():
+    print("Hallo, sqlite-notebook läuft! 🚀")
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
